9c5fa9928eebaf12e8d6919969289706361ebfa2
Peter Dedecker <peter.dedecker@hotmail.com>
2016-08-19 08:45:32 +0200

    Initial support for the lumencor.

--- src/LightSources.hs
+++ src/LightSources.hs
@@ -20,11 +20,14 @@ import Control.Exception
 import Control.Monad
 import Control.Monad.Trans.Except
 import Data.Aeson
+import Data.Bits
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as B
+import Data.IORef
 import Data.List
 import Data.Maybe
 import Data.Monoid
+import Data.Serialize
 import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
@@ -46,6 +49,10 @@ data LightSourceDesc = GPIOLightSourceDesc {
                            clsdName :: !Text
                          , clsdSerialPortName :: !String
                        }
+                     | LumencorLightSourceDesc {
+                           llsdName :: !Text
+                         , llsdSerialPortName :: !String
+                       }
                      | DummyLightSourceDesc {
                            dlsdName :: !Text
                        }
@@ -53,7 +60,14 @@ data LightSourceDesc = GPIOLightSourceDesc {
 
 data LightSource = GPIOLightSource !Text !GPIOPin !Double !GPIOHandles
                  | CoherentLightSource !Text !SerialPort
+                 | LumencorLightSource !Text !SerialPort !(IORef Bool) !(IORef LumencorFilter)
                  | DummyLightSource !Text
+
+data LumencorChannel = LCViolet | LCBlue | LCCyan | LCTeal
+                     | LCGreen | LCYellow | LCRed
+                       deriving (Eq)
+data LumencorFilter = LCGreenFilter | LCYellowFilter
+                      deriving (Eq)
 
 instance ToJSON LightSourceDesc where
     toJSON ls = object ["name" .= lsName ls, "channels" .= lightSourceDescChannels ls,
@@ -63,7 +77,21 @@ instance ToJSON LightSourceDesc where
           lsName :: LightSourceDesc -> Text
           lsName (GPIOLightSourceDesc name _ _) = name
           lsName (CoherentLightSourceDesc name _) = name
+          lsName (LumencorLightSourceDesc name _) = name
           lsName (DummyLightSourceDesc name) = name
+
+lumencorChannels :: [Text]
+lumencorChannels = ["violet", "blue", "cyan", "teal", "green", "yellow", "red"]
+
+lumencorChannelFromName :: Text -> LumencorChannel
+lumencorChannelFromName "violet" = LCViolet
+lumencorChannelFromName "blue" = LCBlue
+lumencorChannelFromName "cyan" = LCCyan
+lumencorChannelFromName "teal" = LCTeal
+lumencorChannelFromName "green" = LCGreen
+lumencorChannelFromName "yellow" = LCYellow
+lumencorChannelFromName "red" = LCRed
+lumencorChannelFromName c = error ("unknown channel" ++ c)
 
 readAvailableLightSources :: IO [LightSourceDesc]
 readAvailableLightSources =
@@ -74,22 +102,27 @@ readAvailableLightSources =
       confFilename = "lightsources.txt"
 
 lightSourceDescChannels :: LightSourceDesc -> [Text]
+lightSourceDescChannels (LumencorLightSourceDesc _ _) = lumencorChannels
 lightSourceDescChannels _ = [""]
 
 lightSourceDescAllowsMultipleChannels :: LightSourceDesc -> Bool
+lightSourceDescAllowsMultipleChannels (LumencorLightSourceDesc _ _) = True
 lightSourceDescAllowsMultipleChannels _ = False
 
 lightSourceDescCanControlPower :: LightSourceDesc -> Bool
 lightSourceDescCanControlPower (GPIOLightSourceDesc _ _ _) = False
 lightSourceDescCanControlPower (CoherentLightSourceDesc _ _) = True
+lightSourceDescCanControlPower (LumencorLightSourceDesc _ _) = True
 lightSourceDescCanControlPower (DummyLightSourceDesc _) = True
 
 lightSourceAllowsMultipleChannels :: LightSource -> Bool
+lightSourceAllowsMultipleChannels (LumencorLightSource _ _ _ _) = True
 lightSourceAllowsMultipleChannels _ = False
 
 lightSourceHasChannel :: LightSource -> Text -> Bool
-lightSourceHasChannel ls channel | channel == "" = True
-                                 | otherwise = False
+lightSourceHasChannel (LumencorLightSource _ _ _ _) ch = ch `elem` lumencorChannels
+lightSourceHasChannel _ channel | channel == "" = True
+                                | otherwise = False
 
 gpioPinsNeededForLightSources :: [LightSourceDesc] -> [GPIOPin]
 gpioPinsNeededForLightSources = map extractPin . filter isGPIOLightSource
@@ -109,6 +142,9 @@ openLightSources gpioHandles descs = sequence $ map openLightSource descs
         openLightSource (CoherentLightSourceDesc name portName) =
             let port = openSerial portName (defaultSerialSettings {commSpeed = CS19200})
             in CoherentLightSource name <$> port
+        openLightSource (LumencorLightSourceDesc name portName) =
+            let port = openSerial portName (defaultSerialSettings {commSpeed = CS115200})
+            in LumencorLightSource name <$> port <*> newIORef False <*> newIORef LCGreenFilter
         openLightSource (DummyLightSourceDesc name) = putStrLn ("opened light source " ++ T.unpack name) >> return (DummyLightSource name)
         openLightSource _ = error "opening unknown type of light source"
 
@@ -116,8 +152,9 @@ closeLightSources :: [LightSource] -> IO ()
 closeLightSources = mapM_ closeLightSource
     where
         closeLightSource (GPIOLightSource _ _ _ _) = return ()
-        closeLightSource (DummyLightSource name) = putStr ("closed light source " ++ T.unpack name) >> return ()
         closeLightSource (CoherentLightSource _ port) = closeSerial port
+        closeLightSource (LumencorLightSource _ port _ _) = closeSerial port
+        closeLightSource (DummyLightSource name) = putStr ("closed light source " ++ T.unpack name) >> return ()
 
 lightSourceName :: LightSource -> Text
 lightSourceName (GPIOLightSource name _ _ _) = name
@@ -150,6 +187,7 @@ validLightSourceChannelsAndPowers ls channels powers
     | null channels = "channels cannot be empty"
     | not (all (\c -> lightSourceHasChannel ls c) channels) = "invalid channel(s)"
     | not (all (\p -> within p 0.0 100.0) powers) = "power outside valid range"
+    | nub channels /= channels = "duplicate channels requested"
     | otherwise = T.empty
 
 activateLightSource :: LightSource -> [Text] -> [Double] -> IO (Either String ())
@@ -159,22 +197,48 @@ activateLightSource ls channels powers
   where
     activateLightSource' (GPIOLightSource _ pin delay handles) _ _ = setPinLevel handles pin High >> threadDelay (floor $ 1e6 * delay) >> return (Right ())
     activateLightSource' (DummyLightSource name) [c] [p] = putStrLn ("activated " ++ T.unpack name ++ " at power " ++ show p ++ " with channel " ++ T.unpack c) >> return (Right ())
-    activateLightSource' (CoherentLightSource name port) _ [power] =
-        catch (powerStr >>= sendAndReadResponse port >> sendAndReadResponse port "L=1\r" >> return (Right ()))
-              (\e -> return (Left (displayException (e :: IOException))))
-        where
-            powerStr :: IO ByteString
-            powerStr = minPowerQ >>= \minPower -> maxPowerQ >>= \maxPower ->
-                      let powerVal = floor (minPower + power / 100.0 * (maxPower - minPower)) :: Int
-                      in return ("P=" <> T.encodeUtf8 (T.pack $ show powerVal) <> "\r")
-            minPowerQ = parseQuery "?MINLP\r"
-            maxPowerQ = parseQuery "?MAXLP\r"
-            parseQuery :: ByteString -> IO Double
-            parseQuery q = sendAndReadResponse port q >>= return . read . filter (`elem` ('.' : ['0' .. '9'])) . T.unpack . T.decodeUtf8
-            sendAndReadResponse :: SerialPort -> ByteString -> IO ByteString
-            sendAndReadResponse prt msg = send port msg >> recvUntilTerminator port
-            recvUntilTerminator port = readFromSerialUntilChar port 10
+    activateLightSource' ls@(CoherentLightSource _ _) _ [power] = activateCoherentLightSource ls power
+    activateLightSource' ls@(LumencorLightSource _ _ _ _) chs ps = activateLumencorLightSource ls (map lumencorChannelFromName chs) ps
     activateLightSource' _ _ _ = error "activating unknown light source"
+
+activateCoherentLightSource :: LightSource -> Double -> IO (Either String ())
+activateCoherentLightSource (CoherentLightSource _ port) power =
+    catch (powerStr >>= sendAndReadResponse port >> sendAndReadResponse port "L=1\r" >> return (Right ()))
+          (\e -> return (Left (displayException (e :: IOException))))
+    where
+        powerStr :: IO ByteString
+        powerStr = minPowerQ >>= \minPower -> maxPowerQ >>= \maxPower ->
+                  let powerVal = floor (minPower + power / 100.0 * (maxPower - minPower)) :: Int
+                  in return ("P=" <> T.encodeUtf8 (T.pack $ show powerVal) <> "\r")
+        minPowerQ = parseQuery "?MINLP\r"
+        maxPowerQ = parseQuery "?MAXLP\r"
+        parseQuery :: ByteString -> IO Double
+        parseQuery q = sendAndReadResponse port q >>= return . read . filter (`elem` ('.' : ['0' .. '9'])) . T.unpack . T.decodeUtf8
+        sendAndReadResponse :: SerialPort -> ByteString -> IO ByteString
+        sendAndReadResponse prt msg = send port msg >> recvUntilTerminator port
+        recvUntilTerminator port = readFromSerialUntilChar port 10
+
+activateLumencorLightSource :: LightSource -> [LumencorChannel] -> [Double] -> IO (Either String ())
+activateLumencorLightSource (LumencorLightSource _ port haveInitRef currFilterRef) channels powers =
+    readIORef haveInitRef >>= \haveInit ->
+    when (not haveInit) (   -- init RS232 and arbitrarily select the green filter
+        send port lumencorEnableRS232Message >>
+        changeFilter LCGreenFilter) >>
+    readIORef currFilterRef >>= \currFilter ->
+    when ((isJust filterForChannel) && (currFilter /= fromJust filterForChannel)) (
+        changeFilter $ fromJust filterForChannel) >>
+    readIORef currFilterRef >>= \possiblyUpdatedFilter ->
+    send port (lumencorIntensityMessage channels powers) >>
+    send port (lumencorEnableMessage channels possiblyUpdatedFilter) >>
+    return (Right ())
+    where
+        changeFilter filter =
+            send port (lumencorFilterMessage filter) >>
+            threadDelay (floor (0.3 * 1.0e6)) >>
+            writeIORef currFilterRef filter
+        filterForChannel | LCGreen `elem` channels  = Just LCGreenFilter
+                         | LCYellow `elem` channels = Just LCYellowFilter
+                         | otherwise                = Nothing
 
 deactivateLightSource :: LightSource -> IO (Either String ())
 deactivateLightSource (GPIOLightSource _ pin delay handles) = setPinLevel handles pin Low >> threadDelay (floor $ 1e6 * delay) >> return (Right ())
@@ -188,6 +252,57 @@ deactivateAllLightSources sources =
     case result of
         Right _ -> return (Right ())
         Left e -> return (Left e)
+
+lumencorEnableRS232Message :: ByteString
+lumencorEnableRS232Message = runPut $
+    mapM_ putWord8 [0x57, 0x02, 0xFF, 0x50, 0x57, 0x03, 0xAB, 0x50]
+
+lumencorFilterMessage :: LumencorFilter -> ByteString
+lumencorFilterMessage = lumencorDisableMessage
+
+lumencorEnableMessage :: [LumencorChannel] -> LumencorFilter -> ByteString
+lumencorEnableMessage channels filter = runPut $
+    mapM_ putWord8 [0x4F, enableByte, 0x50]
+    where
+        enableByte :: Word8
+        enableByte = let channelsEnable = complement $  foldl' (\accum ch -> accum .|. (channelEnableByte ch)) 0 channels
+                     in if (filter == LCGreenFilter)
+                        then channelsEnable .|. 0x10
+                        else channelsEnable .&. complement 0x10
+        channelEnableByte LCViolet = 2^3
+        channelEnableByte LCBlue = 2^5
+        channelEnableByte LCCyan = 2^2
+        channelEnableByte LCTeal = 2^6
+        channelEnableByte LCYellow = 2^1
+        channelEnableByte LCGreen = 2^1
+        channelEnableByte LCRed = 2^0
+
+lumencorIntensityMessage :: [LumencorChannel] -> [Double] -> ByteString
+lumencorIntensityMessage chs ps = mconcat (zipWith lumencorChannelIntensityMessage chs ps)
+
+lumencorChannelIntensityMessage :: LumencorChannel -> Double -> ByteString
+lumencorChannelIntensityMessage ch p = runPut $
+    mapM_ putWord8 [0x53, byte5, 0x03, byte3, byte2, byte1, 0x50]
+    where
+        intensityByte = round ((1.0 - p / 100.0) * 255) -- 255 means no light`
+        dacAndIntensity LCViolet = (0x18, 2^0)
+        dacAndIntensity LCBlue = (0x1A, 2^0)
+        dacAndIntensity LCCyan = (0x18, 2^1)
+        dacAndIntensity LCTeal = (0x1A, 2^1)
+        dacAndIntensity LCGreen = (0x18, 2^2)
+        dacAndIntensity LCYellow = (0x18, 2^2)
+        dacAndIntensity LCRed = (0x18, 2^3)
+        byte5 = fst (dacAndIntensity ch)
+        byte3 = snd (dacAndIntensity ch)
+        byte2 = (intensityByte `shiftR` 4) .|. 0xF0
+        byte1 = intensityByte `shiftL` 4
+
+lumencorDisableMessage :: LumencorFilter -> ByteString
+lumencorDisableMessage filter = runPut $
+    mapM_ putWord8 [0x4F, 0xFF .&. filterSelectByte filter, 0x50]
+    where
+        filterSelectByte LCGreenFilter = 2^4
+        filterSelectByte LCYellowFilter = 0
 
 readFromSerialUntilChar :: SerialPort -> Word8 -> IO ByteString
 readFromSerialUntilChar port c = readUntil' port c B.empty
