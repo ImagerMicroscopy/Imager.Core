ba606419a60d1bfcbd02c4a425f2df22eaad0633
Peter Dedecker <peter.dedecker@hotmail.com>
2016-11-06 07:39:45 -0500

    Possibly faster streaming implementation for ocean optics.

--- src/OODetector.hs
+++ src/OODetector.hs
@@ -3,6 +3,7 @@
 module OODetector where
 
 import Control.Exception
+import Control.Monad
 import Control.Monad.Trans.Except
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as B
@@ -28,19 +29,14 @@ data OODetector = OODetector {
 
 instance Detector OODetector where
     acquireData :: OODetector -> ExposureTime -> Gain -> NMeasurementsToAverage -> IO (Either String AcquiredData)
-    acquireData (OODetector dID fID maybeTrigg corrFunc) expTime _ nSpectraToAverage =
-        (if (isJust maybeTrigg)
-         then acquireTriggeredSpectrum (dID, fID) (fromJust maybeTrigg) expTime nSpectraToAverage
-         else acquireSpectrum (dID, fID) expTime nSpectraToAverage) >>= \spectrum ->
-        case spectrum of
-            Left e  -> return (Left e)
-            Right v -> return (V.map corrFunc v) >>= \corrected ->
-                       getTime Monotonic >>= \timeStamp ->
-                       let nRows = V.length corrected
-                           nCols = 1
-                           bytes = byteStringFromVector corrected
-                           numType = FP64
-                       in return $ Right (AcquiredData nRows nCols timeStamp bytes numType)
+    acquireData det expTime gain nSpectraToAverage = acquireData' det expTime gain nSpectraToAverage True
+
+    acquireStreamingData det expTime gain nMeasurementsToAverage nMeasurements startTime dataMVar =
+        forM_ [1 .. nMeasurements] (\_ ->
+           acquireData' det expTime gain nMeasurementsToAverage False >>= \dat ->
+           case dat of
+             Left e -> error e
+             Right acqData -> addDataToMVar dataMVar startTime [acqData])
 
     getDetectorWavelengths :: OODetector -> IO (Either String (Vector Double))
     getDetectorWavelengths (OODetector dID fID _ _) = getWavelengths dID fID
@@ -48,14 +44,29 @@ instance Detector OODetector where
     getExposureTimeRange :: OODetector -> IO (Either String (ExposureTime, ExposureTime))
     getExposureTimeRange _ = return $ Right (3.8e-3, 1.0)
 
-acquireSpectrum :: (DeviceID, FeatureID) -> Double -> Int -> IO (Either String (Vector Double))
-acquireSpectrum (deviceID, featureID) exposure nSpectra =
+acquireData' :: OODetector -> ExposureTime -> Gain -> NMeasurementsToAverage -> Bool -> IO (Either String AcquiredData)
+acquireData' (OODetector dID fID maybeTrigg corrFunc) expTime _ nSpectraToAverage discardFirstSpectrum =
+  (if (isJust maybeTrigg)
+   then acquireTriggeredSpectrum (dID, fID) (fromJust maybeTrigg) expTime nSpectraToAverage
+   else acquireSpectrum (dID, fID) expTime nSpectraToAverage discardFirstSpectrum) >>= \spectrum ->
+  case spectrum of
+      Left e  -> return (Left e)
+      Right v -> return (V.map corrFunc v) >>= \corrected ->
+                 getTime Monotonic >>= \timeStamp ->
+                 let nRows = V.length corrected
+                     nCols = 1
+                     bytes = byteStringFromVector corrected
+                     numType = FP64
+                 in return $ Right (AcquiredData nRows nCols timeStamp bytes numType)
+
+acquireSpectrum :: (DeviceID, FeatureID) -> Double -> Int -> Bool -> IO (Either String (Vector Double))
+acquireSpectrum (deviceID, featureID) exposure nSpectra discardFirstSpectrum =
     if ((exposure <= 0.0) || (exposure > 1.0) || (nSpectra < 1))
         then return (Left "invalid number of spectra or exposure time")
         else
           runExceptT (
               ExceptT (setIntegrationTimeMicros deviceID featureID integrationMicroseconds) >>
-              ExceptT (measureSpectrum deviceID featureID) >>   -- force a fresh acquisition
+              ExceptT (if discardFirstSpectrum then measureSpectrum deviceID featureID else return (Right V.empty)) >>   -- force a fresh acquisition if needed
               ExceptT (measureAveragedSpectrum deviceID featureID nSpectra))
     where
         integrationMicroseconds = floor (exposure * 1e6)
