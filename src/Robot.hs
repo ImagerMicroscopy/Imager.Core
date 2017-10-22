59960addaa463e196c2cebccd75bccbce4f14fab
Peter Dedecker <peter.dedecker@hotmail.com>
2017-10-22 21:56:20 +0200

    Use polling for robot execution.

--- src/Robot.hs
+++ src/Robot.hs
@@ -1,6 +1,7 @@
-{-# LANGUAGE BangPatterns, OverloadedStrings, ScopedTypeVariables #-}
+{-# LANGUAGE BangPatterns, OverloadedStrings, ScopedTypeVariables, NumDecimals #-}
 module Robot where
 
+import Control.Concurrent
 import Control.Exception
 import Data.Aeson
 import qualified Data.ByteString.Lazy as LB
@@ -17,19 +18,25 @@ import EquipmentTypes
 import MiscUtils
 
 data RobottorRequest = ListRobottorPrograms
-                     | ExecuteRobottorProgram !Text !Bool
+                     | ExecuteRobottorProgram !Text
                      | AbortProgramExecution
+                     | AllowsProgramExecution
+                     | IsExecuting
                      deriving (Show)
 
 data RobottorResponse = OKRobottorResponse
                       | ErrorRobottorResponse !Text
                       | RobottorProgramListResponse ![Text]
+                      | AllowsProgramExecutionResponse !Bool
+                      | IsExecutingResponse !Bool
                       deriving (Show)
 
 instance ToJSON RobottorRequest where
   toJSON ListRobottorPrograms = object ["type" .= ("listprograms" :: Text)]
-  toJSON (ExecuteRobottorProgram prog wait) = object ["type" .= ("executeprogram" :: Text), "programname" .= prog, "waitforcompletion" .= wait]
+  toJSON (ExecuteRobottorProgram prog) = object ["type" .= ("executeprogram" :: Text), "programname" .= prog]
   toJSON AbortProgramExecution = object ["type" .= ("abortprogramexecution" :: Text)]
+  toJSON AllowsProgramExecution = object ["type" .= ("acceptingexternalcommands" :: Text)]
+  toJSON IsExecuting = object ["type" .= ("isexecuting" :: Text)]
 
 instance FromJSON RobottorResponse where
   parseJSON (Object v) =
@@ -41,6 +48,8 @@ instance FromJSON RobottorResponse where
                               "error" -> ErrorRobottorResponse <$> v .: "error"
                               _       -> fail "unknown status"
         "programlist" -> RobottorProgramListResponse <$> v .: "programnames"
+        "acceptingexternalcommands" -> AllowsProgramExecutionResponse <$> v .: "status"
+        "isexecuting" -> IsExecutingResponse <$> v .: "status"
         _             -> fail "can't decode robottor response type"
   parseJSON _ = fail "can't decode robottor response"
 
@@ -65,23 +74,44 @@ isKnownRobot :: [Equipment] -> Text -> Bool
 isKnownRobot mrs name = isJust (lookupRobot mrs name)
 
 listRobotPrograms :: Equipment -> IO [Text]
-listRobotPrograms (Robottor _ ip port) =
-    let serverParams = QueryServerParams ip port (floor 1e6) isCompleteJSONObject decodeJSONObject
-        queryMsg = (LB.toStrict . encode) ListRobottorPrograms
-    in  (queryServer serverParams queryMsg >>= \(RobottorProgramListResponse ps) ->
-      return ps) `catch` \(e :: IOException) -> throw (userError "can't communicate with Robottor program")
+listRobotPrograms r =
+    handleRobottorRequest r ListRobottorPrograms >>= \(RobottorProgramListResponse ps) ->
+    return ps
 
 executeRobotProgram :: Equipment -> Text -> Bool -> IO ()
-executeRobotProgram r@(Robottor _ ip port) progName waitForCompletion = handleRobottorRequest r (ExecuteRobottorProgram progName waitForCompletion)
+executeRobotProgram r progName waitForCompletion =
+    executeRobotProgram' `onException` (abortRobotProgramExecution r)
+    where
+        executeRobotProgram' =
+            _handleRobottorRequest r (ExecuteRobottorProgram progName) >>
+            if (not waitForCompletion)
+            then return ()
+            else waitUntilDone
+        waitUntilDone = robotIsExecuting r >>= \isExecuting ->
+                        if (not isExecuting)
+                        then return ()
+                        else threadDelay 250e3 >> waitUntilDone
 
 abortRobotProgramExecution :: Equipment -> IO ()
-abortRobotProgramExecution r@(Robottor _ ip port) = handleRobottorRequest r AbortProgramExecution
+abortRobotProgramExecution r = _handleRobottorRequest r AbortProgramExecution
 
-handleRobottorRequest :: Equipment -> RobottorRequest -> IO ()
+robotAcceptsExternalCommands :: Equipment -> IO Bool
+robotAcceptsExternalCommands r =
+    putStrLn "testing ext" >>
+    handleRobottorRequest r AllowsProgramExecution >>= \(AllowsProgramExecutionResponse b) -> return b
+
+robotIsExecuting :: Equipment -> IO Bool
+robotIsExecuting r =
+    handleRobottorRequest r IsExecuting >>= \(IsExecutingResponse b) -> return b
+
+handleRobottorRequest :: Equipment -> RobottorRequest -> IO RobottorResponse
 handleRobottorRequest (Robottor _ ip port) req =
     let serverParams = QueryServerParams ip port (floor 1e6) isCompleteJSONObject decodeJSONObject
         queryMsg = (LB.toStrict . encode) req
     in  queryServer serverParams queryMsg `catch` (\(e :: IOException) -> throw (userError "can't communicate with Robottor program")) >>= \response ->
         case response of
-            OKRobottorResponse        -> return ()
             ErrorRobottorResponse err -> throwIO (userError ("error from robottor: " ++ T.unpack err))
+            resp                      -> return resp
+
+_handleRobottorRequest :: Equipment -> RobottorRequest -> IO ()
+_handleRobottorRequest r req = handleRobottorRequest r req >> return ()
