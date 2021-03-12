fc69ebb329265ee6b8ce1c3e0201e0f833cecb9a
Peter Dedecker <peter.dedecker@hotmail.com>
2021-03-12 13:30:57 +0100

    Added missing file.

---
+++ src/MultiModeLasers.hs
@@ -0,0 +1,63 @@
+{-# LANGUAGE OverloadedStrings, BangPatterns #-}
+module MultiModeLasers where
+
+import Control.Concurrent
+import Control.Exception
+import Control.Monad
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as B
+import Data.Char
+import Data.Either
+import Data.Set (Set)
+import qualified Data.Set as S
+import Data.IORef
+import Data.List
+import Data.Maybe
+import Data.Monoid
+import Data.Serialize
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
+import Data.Word
+
+import Equipment
+import EquipmentTypes
+import MiscUtils
+import RCSerialPort
+
+data MultiModeLasers = MultiModeLasers !EqName !SerialPort
+
+data MMLaser = MMRed | MMGreen
+
+initializeMultiModeLasersLightSource :: EquipmentDescription -> IO EquipmentW
+initializeMultiModeLasersLightSource (MultiModeLasersDesc name portName) =
+    let serialSettings = RCSerialPortSettings (defaultSerialSettings {commSpeed = CS19200}) (TimeoutMillis 10000) SerialPortDebugText
+    in  openSerialPort portName serialSettings >>= \port -> threadDelay (floor 2e6) >> -- delay needed, otherwise the arduino won't receive the messages.
+        return (EquipmentW (MultiModeLasers (EqName name) port))
+
+instance Equipment MultiModeLasers where
+    equipmentName (MultiModeLasers n _) = n
+    flushSerialPorts (MultiModeLasers _ p) = flushSerialPort p
+    closeDevice (MultiModeLasers _ port) = closeSerialPort port
+    availableLightSources _ =
+        [LightSourceDescription (LSName "FRETLasers") True True [LSChannelName "Red", LSChannelName "Green"]]
+    activateLightSource (MultiModeLasers _ port) _ chs =
+        forM_ chs (\(LSChannelName n, LSIlluminationPower p) ->
+            case n of
+                "Red"   -> setMMLaserPower port MMRed p
+                "Green" -> setMMLaserPower port MMGreen p
+                _            -> throwIO $ userError "unknown laser type for MultiModeLasers")
+    deactivateLightSource (MultiModeLasers _ port) =
+        setMMLaserPower port MMRed 0.0 >> setMMLaserPower port MMGreen 0.0
+
+setMMLaserPower :: SerialPort -> MMLaser -> Double -> IO ()
+setMMLaserPower port laser power =
+    let lCode = case laser of
+                    MMRed -> fromIntegral (ord 'R')
+                    MMGreen -> fromIntegral (ord 'G')
+        power8 = round (power / 100.0 * 255.0)
+        cmd = B.pack [lCode, power8, fromIntegral (ord '\r')]
+    in  serialWriteAndReadUntilChar port cmd '\r' >>= \resp ->
+        case resp of
+            "OK\r" -> pure ()
+            e      -> throwIO $ userError ("unexpected response from MMLasers: " ++ T.unpack (T.decodeUtf8 e))
