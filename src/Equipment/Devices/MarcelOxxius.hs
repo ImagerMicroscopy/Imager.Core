033d377b53076c9f382d77610fdb20ec181b5762
Peter Dedecker <peter.dedecker@hotmail.com>
2025-02-13 10:14:49 +0100

    Update MarcelOxxius to new AnalogModulation mode.

--- src/Equipment/Devices/MarcelOxxius.hs
+++ src/Equipment/Devices/MarcelOxxius.hs
@@ -35,12 +35,9 @@ data MarcelOxxiusIlluminationMode = MOMGated
 
 initializeMarcelOxxiusLC :: EquipmentDescription -> IO EquipmentW
 initializeMarcelOxxiusLC (MarcelOxxiusLCDesc name oxxPortName ardPortName) =
-    let arSerialSettings = RCSerialPortSettings (defaultSerialSettings {commSpeed = CS19200}) (TimeoutMillis 60000) SerialPortNoDebug
-    in  initializeOxxiusLC' (OxxiusLCDesc name oxxPortName DigitalModulation) >>= \(ox@(OxxiusLC _ oxPort lasers _)) ->
+    let arSerialSettings = RCSerialPortSettings (defaultSerialSettings {commSpeed = CS19200}) (TimeoutMillis 60000) SerialPortDebugText
+    in  initializeOxxiusLC' (OxxiusLCDesc name oxxPortName AnalogModulation) >>= \(ox@(OxxiusLC _ oxPort lasers _)) ->
         openSerialPort ardPortName arSerialSettings >>= \arPort -> threadDelay 2000000 >>
-        handleOxxiusCombinerEchoCommand oxPort "AM=1" >> -- enable analog modulation
-        forM_ (map snd lasers) (\laserParams ->
-            enableAnalogMode oxPort laserParams.oxxType laserParams.oxxIndex) >>
         pure (EquipmentW (MarcelOxxiusLC ox arPort))
         where
             enableAnalogMode :: SerialPort -> OxxiusLaserType -> Int -> IO ()
