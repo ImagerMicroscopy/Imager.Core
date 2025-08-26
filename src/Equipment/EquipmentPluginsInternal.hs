2a7dd976d7a644de92e56c1828d68de15c0b78f8
Peter Dedecker <peter.dedecker@hotmail.com>
2025-08-26 15:43:24 +0200

    New plugin code compiles.

--- src/Equipment/EquipmentPluginsInternal.hs
+++ src/Equipment/EquipmentPluginsInternal.hs
@@ -5,10 +5,15 @@ module Equipment.EquipmentPluginsInternal (
   , addDirectoryToLoaderPath
 ) where
 
+import Control.Exception
 import Control.Monad
+import Data.Aeson hiding (withArray)
 import Data.ByteString(ByteString)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as B
+import qualified Data.ByteString.Lazy as LB
+import qualified Data.ByteString.Unsafe as B
+import Data.Maybe
 import Data.Text(Text)
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
@@ -16,13 +21,17 @@ import qualified Data.Text.Foreign as T
 import qualified Data.Text.IO as T
 import Data.Int
 import Data.List
+import qualified Data.Vector.Storable as V
+import Data.Word
 import Foreign.Ptr
 import Foreign.C.String
 import Foreign.C.Types
+import Foreign.ForeignPtr
 import Foreign.Marshal.Alloc
 import Foreign.Marshal.Array
 import Foreign.Storable
 
+import Camera.SCCameraTypes
 import Equipment.Equipment
 import Equipment.EquipmentTypes
 import Utils.DLLUtils
@@ -55,9 +64,8 @@ data EquipmentPlugin = EquipmentPlugin {
                          , epAcquireSingleImage :: Text -> IO MeasuredImages
                          , epStartAsyncAcquisition :: Text -> IO ()
                          , epStartBoundedAsyncAcquisition :: Text -> Word64 -> IO ()
-                         , epGetOldestImageAsyncAcquired :: Text -> Word -> IO (Maybe MeasuredImages),
+                         , epGetOldestImageAsyncAcquired :: Text -> Word -> IO (Maybe MeasuredImages)
                          , epAbortAsyncAcquisition :: Text -> IO ()
-                         , epGetLastSCCamError :: IO Text
                        }
 
 instance Equipment EquipmentPlugin where
@@ -80,6 +88,7 @@ type ShutdownFunc = IO ()
 type IdentifierFunc = CString -> CUInt -> IO CInt
 type StringListFunc = Ptr CString -> CInt -> CInt -> Ptr CInt -> IO CInt
 type SingleIntPtrFunc = Ptr CInt -> IO CInt
+type GetLastErrorFunc = CString -> CSize -> IO ()
 
 type AvailableLightSourcesFunc = StringListFunc
 type AvailableChannelsFunc      = CString -> Ptr CString -> CInt -> CInt -> Ptr CInt -> Ptr CInt -> Ptr CInt -> IO CInt
@@ -96,20 +105,20 @@ type SupportedStageAxesFunc = Ptr CInt -> Ptr CInt -> Ptr CInt -> IO CInt
 type GetStagePositionFunc = Ptr CDouble -> Ptr CDouble -> Ptr CDouble -> Ptr CInt -> Ptr CInt -> IO CInt
 type SetStagePositionFunc = CDouble -> CDouble -> CDouble -> CInt -> CInt -> IO CInt
 
-type ListConnectedCameraNamesFunc :: StringListFunc
-type GetCameraOptionsFunc :: CString -> Ptr CString -> IO CInt
-type ReleaseOptionsDataFunc :: FunPtr (CString -> IO ())
-type SetCameraOptionFunc :: CString -> CString -> IO CInt
-type GetFrameRateFunc :: CString -> Ptr CDouble -> IO CInt
-type IsConfiguredForHardwareTriggeringFunc :: CString -> Ptr CInt -> IO CInt
-type SetImageOrientationFunc :: CString -> Ptr CInt -> CInt -> IO CInt
-type AcquireSingleImageFunc :: CString -> Ptr (Ptr Word16) -> Ptr CInt -> Ptr CInt -> IO CInt
-type StartAsyncAcquisitionFunc :: CString -> IO CInt
-type StartBoundedAsyncAcquisitionFunc :: CString -> Word64 -> IO CInt
-type GetOldestImageAsyncAcquiredFunc :: CString -> Word32 -> Ptr (Ptr Word16) -> Ptr CInt -> Ptr CInt -> Ptr CDouble -> IO CInt
-type ReleaseImageDataFunc :: FunPtr (Ptr Word16 -> IO ())
-type AbortAsyncAcquisitionFunc :: CString -> IO CInt
-type GetLastSCCamErrorFunc :: CString -> CSize -> IO ()
+type ListConnectedCameraNamesFunc = StringListFunc
+type GetCameraOptionsFunc = CString -> Ptr CString -> IO CInt
+type ReleaseOptionsDataFunc = FunPtr (CString -> IO ())
+type SetCameraOptionFunc = CString -> CString -> IO CInt
+type GetFrameRateFunc = CString -> Ptr CDouble -> IO CInt
+type IsConfiguredForHardwareTriggeringFunc = CString -> Ptr CInt -> IO CInt
+type SetImageOrientationFunc = CString -> Ptr CInt -> CInt -> IO CInt
+type AcquireSingleImageFunc = CString -> Ptr (Ptr Word16) -> Ptr CInt -> Ptr CInt -> IO CInt
+type StartAsyncAcquisitionFunc = CString -> IO CInt
+type StartBoundedAsyncAcquisitionFunc = CString -> Word64 -> IO CInt
+type GetOldestImageAsyncAcquiredFunc = CString -> Word32 -> Ptr (Ptr Word16) -> Ptr CInt -> Ptr CInt -> Ptr CDouble -> IO CInt
+type ReleaseImageDataFunc = FunPtr (Ptr Word16 -> IO ())
+type AbortAsyncAcquisitionFunc = CString -> IO CInt
+type GetLastSCCamErrorFunc = GetLastErrorFunc
 
 foreign import ccall "dynamic" mkInitFunc :: FunPtr InitFunc -> InitFunc
 foreign import ccall "dynamic" mkShutdownFunc :: FunPtr ShutdownFunc -> ShutdownFunc
@@ -200,23 +209,23 @@ loadPlugin libName =
     (if hasStage then (StageName <$> readIdentifier stageNameF) else (pure $ StageName "")) >>= \stageName ->
     (if hasStage then (readSupportedAxesFunc suppAxesF) else pure []) >>= \supportedAxes ->
     listConnectedCameras listConnectedCameraNamesF >>= \connectedCameraNames ->
+    
     EquipmentW <$> pure (EquipmentPlugin eqName shutdownF lightSources (handleActivateLightSource activateLightSourceF)
                                          (handleDeactivateLightSource deactivateLightSourceF)
                                          movableComps (handleSetMoveComponents setMovableComponentsF)
                                          hasStage stageName supportedAxes
                                          (handleGetStagePosition getStagePosF) (handleSetStagePosition setStagePosF)
                                          connectedCameraNames
-                                         (getCameraOptions getCameraOptionsF releaseOptionsDataF)
-                                         (setCameraOption setCameraOptionF)
-                                         (getFrameRate getFrameRateF)
-                                         (isConfiguredForHardwareTriggering isConfiguredForHardwareTriggeringF)
-                                         (setImageOrientation setImageOrientationF)
-                                         (acquireSingleImage acquireSingleImageF)
-                                         (startAsyncAcquisition startAsyncAcquisitionF)
-                                         (startBoundedAsyncAcquisition startBoundedAsyncAcquisitionF)
-                                         (getOldestImageAsyncAcquired getOldestImageAsyncAcquiredF releaseImageDataF)
-                                         (abortAsyncAcquisition abortAsyncAcquisitionF)
-                                         (getLastSCCamError getLastSCCamErrorF))
+                                         (getCameraOptions getLastSCCamErrorF getCameraOptionsF releaseOptionsDataF)
+                                         (setCameraOption getLastSCCamErrorF setCameraOptionF)
+                                         (getFrameRate getLastSCCamErrorF getFrameRateF)
+                                         (isConfiguredForHardwareTriggering getLastSCCamErrorF isConfiguredForHardwareTriggeringF)
+                                         (setImageOrientation getLastSCCamErrorF setImageOrientationF)
+                                         (acquireSingleImage getLastSCCamErrorF acquireSingleImageF releaseImageDataF)
+                                         (startAsyncAcquisition getLastSCCamErrorF startAsyncAcquisitionF)
+                                         (startBoundedAsyncAcquisition getLastSCCamErrorF startBoundedAsyncAcquisitionF)
+                                         (getOldestImageAsyncAcquired getLastSCCamErrorF getOldestImageAsyncAcquiredF releaseImageDataF)
+                                         (abortAsyncAcquisition getLastSCCamErrorF abortAsyncAcquisitionF))
     where
         verifyPluginVersion :: SingleIntPtrFunc -> IO ()
         verifyPluginVersion f = alloca $ \versionPtr ->
@@ -334,64 +343,49 @@ loadPlugin libName =
             \(StagePosition x y z useAF afOffset) ->
                 checkError (f (CDouble x) (CDouble y) (CDouble z) (if (useAF) then 1 else 0) (fromIntegral afOffset))
         
-        listConnectedCameras :: ListConnectedCameraNamesFunc -> IO [Text]
+        listConnectedCameras ::  ListConnectedCameraNamesFunc -> IO [Text]
         listConnectedCameras f = handleStringListFuncT f
 
-        getCameraOptions :: GetCameraOptionsFunc -> ReleaseOptionsDataFunc -> Text -> IO [DetectorProperty]
-        getCameraOptions getOptionsF releaseOptionsF camName =
+        getCameraOptions :: GetLastErrorFunc -> GetCameraOptionsFunc -> ReleaseOptionsDataFunc -> Text -> IO [DetectorProperty]
+        getCameraOptions errF getOptionsF releaseOptionsF camName =
             withCString (T.unpack camName) $ \nameStr ->
             alloca $ \strPtr ->
             poke strPtr nullPtr >>
-            getOptionsF nameStr strPtr >>= \result ->
-            when (result /= 0) (
-                T.unpack <$> getLastSCCamError >>=
-                throwIO . userError) >>
+            checkErrorWithCallback errF (getOptionsF nameStr strPtr) >>
             peek strPtr >>= newForeignPtr releaseOptionsF >>= \fPtr ->
             withForeignPtr fPtr (\dataPtr ->
                 decodeStrict' <$> B.unsafePackCString dataPtr >>= \decoded ->
                 when ((not . isJust) decoded) (print decoded) >>
                 (pure . fromCPList . fromJust) decoded)
         
-        setCameraOption :: SetCameraOptionFunc -> Text -> DetectorProperty -> IO ()
-        setCameraOption f camName option =
+        setCameraOption :: GetLastErrorFunc -> SetCameraOptionFunc -> Text -> DetectorProperty -> IO ()
+        setCameraOption errF f camName option =
             withCString (T.unpack camName) $ \nameStr ->
             B.useAsCString (LB.toStrict $ encode option) $ \optStr ->
-            f nameStr optStr >>= \result ->
-            when (result /= 0) (
-                T.unpack <$> getLastSCCamError >>=
-                throwIO . userError)
+            checkErrorWithCallback errF (f nameStr optStr)
         
-        getFrameRate :: GetFrameRateFunc -> Text -> IO Double
-        getFrameRate f camName =
+        getFrameRate :: GetLastErrorFunc -> GetFrameRateFunc -> Text -> IO Double
+        getFrameRate errF f camName =
             withCString (T.unpack camName) $ \nameStr ->
             alloca $ \frPtr ->
-            f nameStr frPtr >>= \result ->
-            when (result /= 0) (
-                    T.unpack <$> getLastSCCamError >>=
-                    throwIO . userError) >>
+            checkErrorWithCallback errF (f nameStr frPtr) >>
             fromCDouble <$> peek frPtr
         
-        isConfiguredForHardwareTriggering :: IsConfiguredForHardwareTriggeringFunc -> Text -> IO Bool
-        isConfiguredForHardwareTriggering f camName =
+        isConfiguredForHardwareTriggering :: GetLastErrorFunc -> IsConfiguredForHardwareTriggeringFunc -> Text -> IO Bool
+        isConfiguredForHardwareTriggering errF f camName =
             withCString (T.unpack camName) $ \nameStr ->
             alloca $ \isConfPtr ->
-            f nameStr isConfPtr >>= \result ->
-            when (result /= 0) (
-                T.unpack <$> getLastSCCamError >>=
-                throwIO . userError) >>
+            checkErrorWithCallback errF (f nameStr isConfPtr) >>
             peek isConfPtr >>= \isConf ->
             if (isConf == 0)
                 then (pure False)
                 else (pure True)
         
-        setImageOrientation :: SetImageOrientationFunc -> Text -> [OrientationOp] -> IO ()
-        setImageOrientation f camName ops =
+        setImageOrientation :: GetLastErrorFunc -> SetImageOrientationFunc -> Text -> [OrientationOp] -> IO ()
+        setImageOrientation errF f camName ops =
             withCString (T.unpack camName) $ \nameStr ->
             withArray (map encodedOp ops) $ \opsPtr ->
-            f nameStr opsPtr (fromIntegral $ length ops) >>= \errorCode ->
-            when (errorCode /= 0) (
-                T.unpack <$> getLastSCCamError >>=
-                throwIO . userError)
+            checkErrorWithCallback errF (f nameStr opsPtr (fromIntegral $ length ops))
             where
                 encodedOp :: OrientationOp -> CInt
                 encodedOp RotateCWOp = 0
@@ -399,50 +393,38 @@ loadPlugin libName =
                 encodedOp FlipHorizontalOp = 2
                 encodedOp FlipVerticalOp = 3
         
-        acquireSingleImage :: AcquireSingleImageFunc -> Text -> IO MeasuredImages
-        acquireSingleImage f camName =
+        acquireSingleImage :: GetLastErrorFunc -> AcquireSingleImageFunc -> ReleaseImageDataFunc -> Text -> IO MeasuredImages
+        acquireSingleImage errF acqF releaseF camName =
             withCString (T.unpack camName) $ \nameStr ->
             alloca $ \nRowsPtr ->
             alloca $ \nColsPtr ->
             alloca $ \imagePtrPtr ->
             poke imagePtrPtr nullPtr >>
-            f nameStr imagePtrPtr nRowsPtr nColsPtr >>= \result ->
-            when (result /= 0) (
-                T.unpack <$> getLastSCCamError >>=
-                throwIO . userError) >>
-            peek imagePtrPtr >>= newForeignPtr cReleaseImageData >>= \fPtr ->
+            checkErrorWithCallback errF (acqF nameStr imagePtrPtr nRowsPtr nColsPtr) >>
+            peek imagePtrPtr >>= newForeignPtr releaseF >>= \fPtr ->
             fromIntegral <$> peek nRowsPtr >>= \nRows ->
             fromIntegral <$> peek nColsPtr >>= \nCols ->
             pure (MeasuredImages nRows nCols 0.0 (V.unsafeFromForeignPtr0 fPtr (nRows * nCols)))
 
-        startAsyncAcquisition :: StartAsyncAcquisitionFunc -> Text -> IO ()
-        startAsyncAcquisition camName =
+        startAsyncAcquisition :: GetLastErrorFunc -> StartAsyncAcquisitionFunc -> Text -> IO ()
+        startAsyncAcquisition errF f camName =
             withCString (T.unpack camName) $ \nameStr ->
-            f nameStr >>= \result ->
-            when (result /= 0) (
-                T.unpack <$> getLastSCCamError >>=
-                throwIO . userError)
+            checkErrorWithCallback errF (f nameStr)
         
-        startBoundedAsyncAcquisition :: StartBoundedAsyncAcquisitionFunc -> Text -> Word64 -> IO ()
-        startBoundedAsyncAcquisition f camName nImages =
+        startBoundedAsyncAcquisition :: GetLastErrorFunc -> StartBoundedAsyncAcquisitionFunc -> Text -> Word64 -> IO ()
+        startBoundedAsyncAcquisition errF f camName nImages =
             withCString (T.unpack camName) $ \nameStr ->
-            f nameStr nImages >>= \result ->
-            when (result /= 0) (
-                T.unpack <$> getLastSCCamError >>=
-                throwIO . userError)
+            checkErrorWithCallback errF (f nameStr nImages)
 
-        getOldestImageAsyncAcquired :: GetOldestImageAsyncAcquiredFunc -> ReleaseImageDataFunc -> Text -> Word -> IO (Maybe MeasuredImages)
-        getNextAcquiredImage getImageF releaseImageF camName timeoutMillis =
+        getOldestImageAsyncAcquired :: GetLastErrorFunc -> GetOldestImageAsyncAcquiredFunc -> ReleaseImageDataFunc -> Text -> Word -> IO (Maybe MeasuredImages)
+        getOldestImageAsyncAcquired errF getImageF releaseImageF camName timeoutMillis =
             withCString (T.unpack camName) $ \nameStr ->
             alloca $ \imagePtrPtr ->
             alloca $ \nRowsPtr ->
             alloca $ \nColsPtr ->
             alloca $ \timeStampPtr ->
             poke imagePtrPtr nullPtr >>
-            getImageF nameStr (fromIntegral timeoutMillis) imagePtrPtr nRowsPtr nColsPtr timeStampPtr >>= \result ->
-            when (result /= 0) (
-                T.unpack <$> getLastSCCamError >>=
-                throwIO . userError) >>
+            checkErrorWithCallback errF (getImageF nameStr (fromIntegral timeoutMillis) imagePtrPtr nRowsPtr nColsPtr timeStampPtr) >>
             peek imagePtrPtr >>= \imgPtr ->
             if (imgPtr == nullPtr)
                 then return Nothing
@@ -453,21 +435,10 @@ loadPlugin libName =
                     fromCDouble <$> peek timeStampPtr >>= \timeStamp ->
                     pure (Just (MeasuredImages nRows nCols timeStamp (V.unsafeFromForeignPtr0 fPtr (nRows * nCols))))
     
-        abortAsyncAcquisition :: AbortAsyncAcquisitionFunc -> Text -> IO ()
-        abortAsyncAcquisition f camName =
+        abortAsyncAcquisition :: GetLastErrorFunc -> AbortAsyncAcquisitionFunc -> Text -> IO ()
+        abortAsyncAcquisition errF f camName =
             withCString (T.unpack camName) $ \nameStr ->
-            f nameStr >>= \result ->
-            when (result /= 0) (
-                T.unpack <$> getLastSCCamError >>=
-                throwIO . userError)
-        
-        getLastSCCamError :: GetLastSCCamErrorFunc -> IO Text
-        getLastSCCamError f =
-            allocaArray bufSize (\strPtr ->
-                f strPtr (fromIntegral bufSize) >>
-                T.decodeUtf8 <$> B.packCString strPtr)
-            where
-                bufSize = 512
+            checkErrorWithCallback errF (f nameStr)
 
 
 pluginPrinter :: Text -> CString -> IO ()
@@ -513,3 +484,15 @@ readObjects fList fDetail =
 checkError :: IO CInt -> IO ()
 checkError f = f >>= \result ->
                when (result /= 0) (error "error executing plugin")
+
+checkErrorWithCallback :: GetLastSCCamErrorFunc -> IO CInt -> IO ()
+checkErrorWithCallback errFunc f =
+    f >>= \result ->
+    when (result /= 0) (
+        allocaArray bufSize (\strPtr ->
+            errFunc strPtr (fromIntegral bufSize) >>
+            T.unpack . T.decodeUtf8 <$> B.packCString strPtr) >>=
+            throwIO . userError
+    )
+    where
+        bufSize = 512
